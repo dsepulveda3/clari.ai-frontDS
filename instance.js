a6aadd2a9289457c3e1d488e94b12873b1d28a18
lucasvsj <lfvansintjan@uc.cl>
2023-03-22 16:24:46 -0300

    fix: Updated staging URL, added /api route

--- instance.js
+++ instance.js
@@ -14,7 +14,7 @@ const isLocalhost = false
 // const isLocalhost = true
 
 // export const mainUrl = `https://clariapp.herokuapp.com/api`
-export const mainUrl = `https://clari-staging.herokuapp.com/`
+export const mainUrl = `https://clari-staging.herokuapp.com/api`
 // export const mainUrl = `http://localhost:8080/api`
 
 const instance = axios.create({
